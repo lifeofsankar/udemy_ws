8c05b01b9ca75eeccc9c6243d37b3031d90d39ff
lifeofsankar <jaisankarjaikishan369@gmail.com>
2025-08-13 09:49:28 +0530

    Section 6 of ROS2 Course Completed

--- src/py_pkg/py_pkg/number_counter.py
+++ src/py_pkg/py_pkg/number_counter.py
@@ -2,14 +2,20 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
+from example_interfaces.srv import SetBool
  
  
 class NumCountNode(Node): 
     def __init__(self):
         super().__init__("number_counter") 
         self.counter_ = 0
-        self.number_count_publisher_ = self.create_publisher(Int64, "number_count", 10)
-        self.number_subscriber_=self.create_subscription(Int64, "number", self.callback_number, 10)
+        self.number_count_publisher_ = self.create_publisher(
+            Int64, "number_count", 10)
+        self.number_subscriber_=self.create_subscription(
+            Int64, "number", self.callback_number, 10)
+        self.reset_counter_service_ = self.create_service(
+            SetBool, "reset_counter", self.callback_reset_counter
+        )
         self.get_logger().info("Number Counter conneted.")
     
     def callback_number(self, msg: Int64):
@@ -17,6 +23,16 @@ class NumCountNode(Node):
         new_msg = Int64()
         new_msg.data = self.counter_
         self.number_count_publisher_.publish(new_msg)
+        
+    def callback_reset_counter(self, request: SetBool.Request, response: SetBool.Response):
+        if request.data:
+            self.counter_= 0
+            response.success = True
+            response.message = "Counter has been reset"
+        else:
+            response.success = False
+            response.message = "Counter has not been reset"
+        return response
  
  
 def main(args=None):
